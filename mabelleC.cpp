5a3b365cd9510d03115c086fd21d1d1729dd9649
Mabelle <mabellecruz@gmail.com>
2019-09-25 17:07:56 -0700

    Fixed my credits postion

--- mabelleC.cpp
+++ mabelleC.cpp
@@ -1,26 +1,32 @@
+/* mabelleC.cpp
+ *
+ * Mabelle Cruz
+ * CS335: Fall 2019
+ *
+ */
+
 #include <GL/glx.h>
 #include "fonts.h"
 
-void mabelleC(int x, int y, GLuint id)
+void showMabelleCredits(int x, int y, GLuint id)
 {
-    Rect r;
-    float wid = 120.0f;
-
-    // Displays my name
-    r.bot = 150;
-    r.left = 150;
-    r.center = 0;
-    ggprint16(&r, 16, 0x0, "Mabelle Cruz");
-    //                  // Displays my picture
-    glColor3ub(255, 255, 255);
-    glPushMatrix();
-    glTranslatef((float)x, (float)y, 0);
-    glBindTexture(GL_TEXTURE_2D, id);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid/2, -wid/2);
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid/2,  wid/2);
-    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid/2,  wid/2);
-    glTexCoord2f(1.0f, 1.0f); glVertex2i( wid/2, -wid/2);
-    glEnd();
-    glPopMatrix();
+	Rect r;
+	float wid = 100.0f;
+	// Displays my name
+	r.bot = 90;
+	r.left = 90;
+	r.center = 0;
+	ggprint16(&r, 16, 0x0, "Mabelle Cruz");
+	// Displays my picture
+	glColor3ub(255, 255, 255);
+	glPushMatrix();
+	glTranslatef((float)x, (float)y, 0);
+	glBindTexture(GL_TEXTURE_2D, id);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid/2, -wid/2);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid/2,  wid/2);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i( wid/2,  wid/2);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i( wid/2, -wid/2);
+	glEnd();
+	glPopMatrix();
 }
