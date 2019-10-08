5d94ea7c0898f6e12cdf2eca9566ee94b2b6228f
Student <student@lab-240-001.cs.csubak.edu>
2019-10-08 11:16:26 -0700

    Added showScore()

--- mabelleC.cpp
+++ mabelleC.cpp
@@ -30,3 +30,25 @@ void showMabelleCredits(int x, int y, GLuint id)
 	glEnd();
 	glPopMatrix();
 }
+
+void showScores(int x, int y, GLuint id)
+{
+	Rect r;
+	float wid = 100.0f;
+	r.bot = 200;
+	r.left = 200;
+	r.center = 0;
+	ggprint16(&r, 16, 0x0, " ");
+	glColor3ub(255, 255, 255);
+	glPushMatrix();
+	glTranslatef((float)x, (float)y, 0);
+	glBindTexture(GL_TEXTURE_2D, id);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid/2, -wid/2);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid/2,  wid/2);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i( wid/2,  wid/2);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i( wid/2, -wid/2);
+	glEnd();
+	glPopMatrix();
+}
+
