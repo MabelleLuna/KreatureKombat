cb45ebf10e72b8df000f7e327d6b974910891fd6
Mabelle <mabellecruz@gmail.com>
2019-10-26 19:25:24 -0700

    Added function to try to draw a sprite

--- mabelleC.cpp
+++ mabelleC.cpp
@@ -58,3 +58,38 @@ void showScores(int x, int y, GLuint id)
 	glPopMatrix();
 }
 
+
+void drawSprite(GLuint texture, Image &sprite, 
+float width, float height, float xpos, float ypos)
+{
+	glPushMatrix();
+	glColor3f(1.0, 1.0, 1.0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glEnable(GL_ALPHA_TEST);
+	glAlphaFunc(GL_GREATER, 0.0f);
+	glColor4ub(255, 255, 255, 255);
+	int ix = sprite.frame % sprite.columns;
+	int iy = sprite.animation;
+	float tx = (float) ix / sprite.columns;
+	float ty = (float) iy / sprite.rows;
+	float swidth = (float) 1.0 / sprite.columns;
+	float sheight = (float) 1.0 / sprite.rows;
+	
+	if (sprite.frame >= sprite.columns) { iy = 1; }
+	
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_QUADS);
+	glTexCoord2f(tx, ty + sheight);
+	glVertex2i(xpos - width, ypos - height);
+	glTexCoord2f(tx, ty);
+	glVertex2i(xpos - width, ypos + height);
+	glTexCoord2f(tx + swidth, ty);
+	glVertex2i(xpos + width, ypos + height);
+	glTexCoord2f(tx + swidth, ty + sheight);
+	glVertex2i(xpos + width, ypos - height);
+	glEnd();
+	glPopMatrix();
+	
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glDisable(GL_ALPHA_TEST);
+}
