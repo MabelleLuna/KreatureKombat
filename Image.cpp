2f4a63ee6598a2482cfa94d3c95265060df4a49b
Mabelle <mabellecruz@gmail.com>
2019-10-26 19:11:52 -0700

    Removed some stuff

--- Image.cpp
+++ Image.cpp
@@ -1,23 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-class Image {
-	public:
-		int width;
-		int height;
-		int rows;
-		int columns;
-		int frame;
-		int frameCounter;
-		int animation;
-		unsigned char *data;
-		const char *file;
-		Image(const char* f, int r = 0, int c = 0);
-		~Image();
-};
-
+#include "Image.h"
 
 Image::Image(const char *fname, int row, int col) {
 	file = fname;
