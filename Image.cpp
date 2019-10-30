a640ce548b7185ed1a8feeeea80c246d41aa6145
Oscar Rodriguez <orodriguez@odin.cs.csubak.edu>
2019-10-30 10:42:37 -0700

    added a couple more functions to Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -48,3 +48,39 @@ Image::Image(const char *fname, int row, int col) {
 Image::~Image() {
 	delete [] data;
 }
+
+void Image::GenerateGLTexture(GLuint & texture)
+{
+
+        glGenTextures( 1, &texture);
+            glBindTexture( GL_TEXTURE_2D, texture);
+
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+
+                        glTexImage2D( GL_TEXTURE_2D, 0, 3, this->width, this->height, 0, GL_RGB,
+                                                    GL_UNSIGNED_BYTE, this->data);
+                            glBindTexture( GL_TEXTURE_2D, 0);
+}
+void Image::GenerateGLTextureALPHA(GLuint & texture, const char * img)
+{
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load( img, &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        //glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+}
